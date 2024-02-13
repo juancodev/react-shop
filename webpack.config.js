45ed1b1a527af877e04c0c75ee1c6cbc9c94cd2d
juancodev <montillasanchezjuancarlos@gmail.com>
2024-02-13 00:52:40 -0400

    🐛 Problem with file bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/react-shop',
   },
   mode: 'production',
   resolve: {
